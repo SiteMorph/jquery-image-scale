d25a5cc41e1d520c343e7c5327f6d4b45a2cbcc6
Damien Allison <damien@sitemorph.net>
2013-09-16 13:45:37 +0100

    updated function call, return etc.

--- jquery-image-scale.js
+++ jquery-image-scale.js
@@ -13,67 +13,68 @@ if (typeof jQuery == 'undefined') {
   throw 'jquery-image-scale plugin required jQuery to be loaded';
 }
 
-jQuery.fn.scaleImageToParent = function(parentSelector) {
+(function(jQuery) {
 
-  if ('undefined' === typeof(parentSelector)) {
-    parentSelector = 'div';
-  }
+  jQuery.fn.scaleImageToParent = function(parentSelector) {
 
-  this.each(function() { 
-  
-    var image = $(this);
-    var parents = image.parents(parentSelector);
-    if (1 > parents.length) {
-      return true; 
+    if ('undefined' === typeof(parentSelector)) {
+      parentSelector = 'div';
     }
-    var container = $(parents[0]);
 
-    image.load(function() {
-
-      var width = image.width();
-      var height = image.height();
-      if (0 == width || 0 == height) {
-        // still not loaded
-        return true;
+    return this.each(function() { 
+    
+      var image = $(this);
+      var parents = image.parents(parentSelector);
+      if (1 > parents.length) {
+        return true; 
       }
+      var container = $(parents[0]);
 
-      console.log('Container: ' + container.width() + ' x ' + container.height());
-      console.log('Image: ' + width + ' x ' + height);
+      image.load(function() {
 
-      // stage 1: resize in the width to ensure the width is covered.
-      if (width < container.width()) {
-        var scale = container.width() / width;
-        width = scale * width;
-        height = scale * height;
-      } else { // scale down if larger
-        var scale = container.width() / width;
-        width = scale * width;
-        height = scale * height;
-      }
+        var width = image.width();
+        var height = image.height();
+        if (0 == width || 0 == height) {
+          // still not loaded
+          return true;
+        }
 
-      // stage 2: if the image is still too short scale up to height
-      if (height < container.height()) {
-        var scale = container.height() / height;
-        width = scale * width;
-        height = scale * height;
-      }
+        // stage 1: resize in the width to ensure the width is covered.
+        if (width < container.width()) {
+          var scale = container.width() / width;
+          width = scale * width;
+          height = scale * height;
+        } else { // scale down if larger
+          var scale = container.width() / width;
+          width = scale * width;
+          height = scale * height;
+        }
 
-      width = Math.floor(width);
-      height = Math.floor(height);
+        // stage 2: if the image is still too short scale up to height
+        if (height < container.height()) {
+          var scale = container.height() / height;
+          width = scale * width;
+          height = scale * height;
+        }
 
-      // stage 3: determine the left offset to center the image horizontally
-      var leftAlign = - Math.floor((width - container.width()) / 2);
-      var topAlign = - Math.floor((height - container.height()) / 2);
+        width = Math.floor(width);
+        height = Math.floor(height);
+
+        // stage 3: determine the left offset to center the image horizontally
+        var leftAlign = - Math.floor((width - container.width()) / 2);
+        var topAlign = - Math.floor((height - container.height()) / 2);
 
 
-      // write the image css
-      image.css('width', width);
-      image.css('height', height);
-      image.css('margin-left', leftAlign);
-      image.css('margin-top', topAlign);
+        // write the image css
+        image.css('width', width);
+        image.css('height', height);
+        image.css('position', 'relative');
+        image.css('left', leftAlign);
+        image.css('top', topAlign);
+      });
+      image.load();
     });
-    image.load();
-  });
 
-  return this;
-}
+    return this;
+  }
+})(jQuery);
